22c3d47b8a17164b54f369203daafa7ffe472588
Krishna Kaushik <46679308+krishnakaushik25@users.noreply.github.com>
2021-09-04 18:53:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://krishna_11:mc8jbgXv3Fiiq3Cg@cluster0.fpq7t.mongodb.net/merng?retryWrites=true&w=majority",
+    "mongodb+srv://krishna_11:9YT0sj870kqCOLxK@cluster0.fpq7t.mongodb.net/merng?retryWrites=true&w=majority",
     
   SECRET_KEY: 'some very secret key'
 };
